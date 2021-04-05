416090023ef18221c4b2a65d8848394f838d80a7
Konstantin Krastev <kkonstantin@vmware.com>
2021-04-05 23:05:20 +0300

    setuptools integration

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ssm-port-forwarding',
+    version='0.0.1',
+    packages=find_packages(),
+    url='https://gitlab.bit9.local/kkonstantin/ssm-port-forwarding',
+    license='MIT',
+    author='Konstantin Krastev',
+    author_email='kkonstantin@vmware.com',
+    description='session manager remote port forwarding utility',
+    include_package_data=True,
+    install_requires=[
+        'Click',
+        'pexpect',
+        'boto3',
+        'packaging'
+    ],
+    entry_points='''
+        [console_scripts]
+        ssmpfwd=ssmpfwd.cli.cli:cli
+    ''',
+
+)
